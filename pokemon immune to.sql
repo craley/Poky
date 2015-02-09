202d8236140bc8dc1acfa652f00477c69e0e6bea
Thomas Sweeney <tsweeney256@gmail.com>
2015-02-08 21:38:38 -0500

    implemented the typing system

---
+++ pokemon immune to.sql
@@ -0,0 +1,9 @@
+select immunityidType
+from Type
+left join Type_Immune_To
+on idType=Type_Immune_To.targetidType
+left join Type_Resistant_To
+on idType=Type_Resistant_To.targetidType
+left join Type_Weak_To
+on idType=Type_Weak_To.targetidType
+where idType=? and resistanceidType is not null
